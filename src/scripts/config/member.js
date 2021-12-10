5be3569ccebf20d506eabb68a00f0c8317a4b144
Vansh186 <78458980+Vansh186@users.noreply.github.com>
2021-12-10 09:55:37 +0530

    Create member.js

---
+++ src/scripts/config/member.js
@@ -0,0 +1,120 @@
+export default [{
+        "id": "lightless",
+        "intro": "Web 安全 / 安全工程师 / 半吊子开发 / 半吊子安全研究",
+        "avatar": require("../../images/avatar/lightless.jpg"),
+        "url": "https://lightless.me/"
+    },
+    {
+        "id": "Edward_L",
+        "intro": "13 级 HDUISA 会长 / Web 安全 / 华为安全部门 / 二进制安全，fuzz，符号执行方向研究",
+        "avatar": require("../../images/avatar/edward_L.jpg"),
+        "url": "#"
+    },
+    {
+        "id": "逆风",
+        "intro": "13 级菜鸡 / 大数据打杂",
+        "avatar": require("../../images/avatar/deadwind4.jpeg"),
+        "url": "https://github.com/deadwind4"
+    },
+    {
+        "id": "陈斩仙",
+        "intro": "什么都不会 / 咸鱼研究生 / <del>安恒</del>、<del>长亭</del> / SJTU",
+        "avatar": require("../../images/avatar/chenzhanxian.jpg"),
+        "url": "https://mxgcccc4.github.io/"
+    },
+    {
+        "id": "Eric",
+        "intro": "渗透 / 人工智能 / 北师大博士在读",
+        "avatar": require("../../images/avatar/eric.jpg"),
+        "url": "https://3riccc.github.io"
+    },
+    {
+        "id": "Explorer",
+        "intro": "14 级 HDUISA 会长 / 二进制安全 / 曾被 NULL、TD、蓝莲花等拉去凑人数 / 差点没毕业 / 长亭安研",
+        "avatar": require("../../images/avatar/Explorer.jpg"),
+        "url": "#"
+    },
+    {
+        "id": "Aklis",
+        "intro": "14 级 HDUISA 副会长 / 二次元 / 拼多多安全工程师",
+        "avatar": require("../../images/avatar/aklis.jpg"),
+        "url": "#"
+    },
+    {
+        "id": "Sysorem",
+        "intro": "14 级网安协会会长 / HDUISA 成员 / Web 安全 / Freebuf 安全社区特约作者 / FSI2015Freebuf 特邀嘉宾",
+        "avatar": require("../../images/avatar/sysorem.jpg"),
+        "url": "#"
+    },
+    {
+        "id": "Hcamael",
+        "intro": "13 级 / 知道创宇 404 安全研究员 / 现在 Nu1L 划划水 / IoT、Web、二进制漏洞，密码学，区块链都看得懂一点，但啥也不会",
+        "avatar": require("../../images/avatar/hcamael.jpg"),
+        "url": "#"
+    },
+    {
+        "id": "LoRexxar",
+        "intro": "14 级 / Web 🐶 / 杭电江流儿 / 自走棋主教守门员",
+        "avatar": require("../../images/avatar/lorexxar.jpg"),
+        "url": "https://lorexxar.cn/"
+    },
+    {
+        "id": "A1ex",
+        "intro": "14 级网安协会副会长 / Web 安全",
+        "avatar": require("../../images/avatar/alex.jpg"),
+        "url": "#"
+    },
+    {
+        "id": "Ahlaman",
+        "intro": "14 级网安协会副会长 / 无线安全",
+        "avatar": require("../../images/avatar/ahlaman.jpg"),
+        "url": "#"
+    },
+    {
+        "id": "Undefinedv",
+        "intro": "15 级网安协会会长 / Web 安全",
+        "avatar": require("../../images/avatar/undefinedv.jpg"),
+        "url": "#"
+    },
+    {
+        "id": "Spine",
+        "intro": "逆向 / 二进制安全",
+        "avatar": require("../../images/avatar/spine.jpg"),
+        "url": "#"
+    },
+    {
+        "id": "Tata",
+        "intro": "二进制 CGC 入门水准 / 半吊子爬虫与反爬虫",
+        "avatar": require("../../images/avatar/tata.jpg"),
+        "url": "#"
+    },
+    {
+        "id": "Airbasic",
+        "intro": "Web 安全 / 长亭科技安服部门 / TSRC 2015 年年度英雄榜第八、2016 年年度英雄榜第十三",
+        "avatar": require("../../images/avatar/airbasic.jpg"),
+        "url": "#"
+    },
+    {
+        "id": "jibo",
+        "intro": "15 级 / 什么都不会的开发 / 打什么都菜",
+        "avatar": require("../../images/avatar/jibo.jpg"),
+        "url": "#"
+    },
+    {
+        "id": "Processor",
+        "intro": "15 级 Vidar 会长 / 送分型逆向选手 / 13 段剑纯 / 差点没毕业 / 阿斯巴甜有点甜",
+        "avatar": require("../../images/avatar/Processor.jpeg"),
+        "url": "https://processor.pub/"
+    },
+    {
+        "id": "HeartSky",
+        "intro": "15 级 / 挖不到洞 / 打不动 CTF / 内网渗透不了 / 工具写不出",
+        "avatar": require("../../images/avatar/heartsky.jpg"),
+        "url": "http://heartsky.info"
+    },
+    {
+        "id": "Minygd",
+        "intro": "15 级 / 删库跑路熟练工 / 没事儿拍个照 / 企鹅",
+        "avatar": require("../../images/avatar/mingy.jpg"),
+        "url": "#"
+    }
